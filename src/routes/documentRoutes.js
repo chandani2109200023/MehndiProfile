a8f2fc81719898de8040ad3c16aadc56e00aad0c
Ritikakri935 <20190802065@dypiu.ac.in>
2025-03-18 11:05:15 +0530

    documented

--- src/routes/documentRoutes.js
+++ src/routes/documentRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const documentController = require('../controllers/documentController');
 const authAdmin = require('../middleware/authAdmin');
-const authUser = require('../middleware/auth'); // Middleware for authenticated users
 
 const upload = documentController.upload.fields([
     { name: 'passbookImage', maxCount: 1 },
